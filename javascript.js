ca1117b4d5d847ea0b7b930959ef2c99a794abc4
Anderson Luis Furlan <alsfurlan@gmail.com>
2021-05-13 20:05:14 -0300

    tipagem dinâmica

--- javascript.js
+++ javascript.js
@@ -1,26 +1,10 @@
-var v1 = 1;
+var v1 = 'string';
 console.log(v1, '-', typeof(v1));
-
-var v2 = 3.1415;
-console.log(v2, '-', typeof(v2));
-
-var v3 = 'a';
-console.log(v3, '-', typeof(v3));
-
-var v4 = 'string';
-console.log(v4, '-', typeof(v4));
-
-var v5 = true;
-console.log(v5, '-', typeof(v5));
-
-var v6 = false;
-console.log(v6, '-', typeof(v6));
-
-var v7 = null;
-console.log(v7, '-', typeof(v7));
-
-var v8 = undefined;
-console.log(v8, '-', typeof(v8));
-
-var v9;
-console.log(v9, '-', typeof(v9));
+v1 = true;
+console.log(v1, '-', typeof(v1));
+v1 = 2;
+console.log(v1, '-', typeof(v1));
+v1 = undefined;
+console.log(v1, '-', typeof(v1));
+v1 = parseInt('teste');
+console.log(v1, '-', typeof(v1));
