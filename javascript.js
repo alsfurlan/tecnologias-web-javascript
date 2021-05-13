b80779e824964f822bcb975a6e8cbd5aed902d15
Anderson Luis Furlan <alsfurlan@gmail.com>
2021-05-13 20:32:11 -0300

    declaração de variáveis: var versus let

--- javascript.js
+++ javascript.js
@@ -1,30 +1,20 @@
-var v1 = [];
-console.log(v1, '-', typeof(v1));
+var v1 = 'string';
+let v2 = 'string';
 
-var v2 = new Array();
-console.log(v2, '-', typeof(v2));
+// var não respeita escopo
+if(true) {
+    var v = 'teste';
+}
+console.log(v);
 
-v1.push('string');
-v1.push(true);
-v1.push(2);
+// let existe apenas dentro do escopo
+if(true) {
+    let l = 'teste';
+}
+console.log(l);
 
-console.log(v1);
-
-const ultimo = v1.pop();
-console.log(ultimo);
-console.log(v1);
-
-const primeiro = v1.shift();
-console.log(primeiro);
-console.log(v1);
-
-v2.push(1, 2, 3, 4, 5);
-console.log(v2);
-
-v2.splice(0, 3);
-console.log(v2);
-
-v2.splice(0, 1);
-console.log(v2);
-
-
+function soma(n1, n2) {
+    var total = n1+n2;
+}
+soma(1, 2);
+console.log(total);
