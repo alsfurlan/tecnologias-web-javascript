09635a544e39716d84aa89eee4e27ca8246c311a
Anderson Luis Furlan <alsfurlan@gmail.com>
2021-05-13 21:21:31 -0300

    estrutura de repetição: while

--- javascript.js
+++ javascript.js
@@ -1,10 +1,5 @@
-const variavel = 'teste1';
 
-switch(variavel) {
-    case 'teste1':
-        console.log('teste1');
-        break;
-    case 'teste2':
-        console.log('teste2');
-        break;
+let i = 0;
+while(i < 10) {
+    console.log(i++);
 }
